449f4a5727812c6d4a8be67b76c1aa97c83189d5
ddicara-gb <ddicara@gnubio.com>
2014-06-12 16:28:18 -0400

    still testing

--- src/controller.py
+++ src/controller.py
@@ -78,13 +78,14 @@ def api(version, name):
         return jsonify(swagger_api_declaration)
     abort(404)
 
-@app.route('%s/<version>/<name>/<path:path>' % API_BASE_ROUTE, methods=['GET', 'POST', 'OPTIONS'])
+@app.route('%s/<version>/<name>/<path:path>' % API_BASE_ROUTE, methods=['GET', 'POST'])
 @crossdomain(origin=ORIGIN)
 def function(version, name, path):
     t = time.time()
     version = version.lower()
     api_function = API_MANAGER.get_api_function(name, version, path, request.method)
     if api_function:
+        print request.files
         
         # For example path "MeltingTemperatures/IDT/{name}/{sequence}", 
         # dynamic_path_fields would be [<name>, <sequence>]

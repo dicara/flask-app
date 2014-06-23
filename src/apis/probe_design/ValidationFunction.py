d6d5c789298aaa70f6b56b0279be7fa884004d09
ddicara-gb <ddicara@gnubio.com>
2014-06-23 10:30:27 -0400

    debug prints

--- src/apis/probe_design/ValidationFunction.py
+++ src/apis/probe_design/ValidationFunction.py
@@ -71,8 +71,9 @@ class ValidationFunction(AbstractGetFunction):
         num               = params_dict[ParameterFactory.integer("num", "Minimum number of probes for a target.",
                                                default=3, minimum=1)]
         print "Probes: %s" % probes_file_uuid
-        path = cls._DB_CONNECTOR.find_one(PROBES_COLLECTION, UUID, probes_file_uuid)
-        print "Probes record: " % path[FILEPATH]
+        path, one = cls._DB_CONNECTOR.find_one(PROBES_COLLECTION, UUID, probes_file_uuid)
+        print "Probes record: %s" % path
+        print "Probes one: %s" % one
         print "Targets: %s" % targets_file_uuid
         print "absorb: %s" % absorb
         print "num: %s" % num

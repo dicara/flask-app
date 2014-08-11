5fbdf765654c0ccd79fbc0a8488e2f5bc45c7f5a
ddicara-gb <ddicara@gnubio.com>
2014-08-11 10:24:02 -0400

    Still experimenting with packaging.

--- setup.py
+++ setup.py
@@ -41,11 +41,7 @@ setup(
                                           'flask_api = src.server:main',
                                          ]},
       packages         = find_packages(),
-      dependency_links = [
-                          'git+ssh://git@github.com/ddicara-gb/idtAnalyzer.git@packaging#egg=idt-analyzer-0.1',
-                          ],
       install_requires = [
-                          'setuptools=5.5.1',
                           'pymongo==2.7',
                           'pyyaml==3.10',
                           'simplejson==3.3.1',
@@ -56,6 +52,9 @@ setup(
                           'futures==2.1.6',
                           'idt-analyzer',
                          ],
+      dependency_links = [
+                          'git+ssh://git@github.com/ddicara-gb/idtAnalyzer.git@packaging#egg=idt-analyzer-0.1',
+                          ],
       package_data     = {'': ['src/templates/*'],},
       description      = _LONG_DESCRIPTION,
       test_suite       = 'nose.collector',

9b7dc44ed1ceaf77a47cdd30f7a9d966d7fda883
ddicara-gb <ddicara@gnubio.com>
2016-02-13 23:32:50 -0500

    Remove deprecated arguments from assay caller and update test.

--- bioweb_api/tests/apis/secondary_analysis/TestSecondaryAnalysisAPI.py
+++ bioweb_api/tests/apis/secondary_analysis/TestSecondaryAnalysisAPI.py
@@ -20,7 +20,6 @@ limitations under the License.
 #=============================================================================
 # Imports
 #=============================================================================
-import filecmp
 import os
 import shutil
 import time
@@ -33,17 +32,18 @@ from bioweb_api.tests.test_utils import post_data, get_data, \
     delete_data, add_url_argument
 from bioweb_api.utilities import io_utilities
 from bioweb_api.apis.ApiConstants import UUID, FIDUCIAL_DYE, ASSAY_DYE,\
-    NUM_PROBES, TRAINING_FACTOR, THRESHOLD, OUTLIERS, DYE_LEVELS, PLOT, \
-    JOB_NAME, JOB_TYPE_NAME, JOB_TYPE, ARCHIVE, COV_TYPE, DEVICE, DYES, \
+    NUM_PROBES, TRAINING_FACTOR, DYE_LEVELS, PLOT, \
+    JOB_NAME, JOB_TYPE_NAME, JOB_TYPE, ARCHIVE, DEVICE, DYES, \
     STATUS, SUBMIT_DATESTAMP, START_DATESTAMP, FINISH_DATESTAMP, \
     URL, CONFIG_URL, REPORT, RESULT, CONFIG, KDE_PLOT, SCATTER_PLOT, \
-    FILTERED_DYES
-from bioweb_api import app, HOME_DIR, TMP_PATH, PA_PROCESS_COLLECTION
+    FILTERED_DYES, IGNORED_DYES, PF_TRAINING_FACTOR, UI_THRESHOLD,\
+    CALC_DROP_PROB, PA_PROCESS_UUID, PLOT_URL, REPORT_URL
+from bioweb_api import app, HOME_DIR, TMP_PATH, PA_PROCESS_COLLECTION, SA_IDENTITY_COLLECTION
 from bioweb_api.apis.secondary_analysis.IdentityPostFunction import IDENTITY
 from bioweb_api.apis.secondary_analysis.AssayCallerPostFunction import ASSAY_CALLER
+from secondary_analysis.constants import AC_TRAINING_FACTOR
 
 from bioweb_api.DbConnector import DbConnector
-from secondary_analysis.constants import COVARIANCE_TYPES
 
 #=============================================================================
 # Private Static Variables
@@ -61,13 +61,9 @@ _IDENTITY_JOB_NAME     = "test_identity"
 _ASSAY_CALLER_JOB_NAME = "test_assay_caller"
 _FIDUCIAL_DYE          = "joe"
 _ASSAY_DYE             = "fam"
-_AC_NUM_PROBES         = 100
+_AC_NUM_PROBES         = 8
 _ID_NUM_PROBES         = 0
-_AC_TRAINING_FACTOR    = 1
 _TRAINING_FACTOR       = 10
-_THRESHOLD             = 2500.0
-_OUTLIERS              = False
-_COV_TYPE              = COVARIANCE_TYPES[-1]
 _DYE_LEVELS            = "594:4,633:3,cy5.5:4,pe:2,pe-cy7:2,IF790:2"
 
 # ABL r2 Lot 0003 reagents Run
@@ -81,43 +77,12 @@ io_utilities.safe_make_dirs(HOME_DIR)
 io_utilities.safe_make_dirs(TMP_PATH)
 
 #=============================================================================
-# Private Static Variables
-#=============================================================================
-
-#=============================================================================
 # Class
 #=============================================================================
 class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        ########################################################################
-        # Identity Records
-        ########################################################################
-        cls._pa_process_ac_uuid = "402895fd-6a44-411c-817b-cba84c2abb8c"
-        cls._ac_record = {
-                          JOB_NAME : "test_binary",
-                          JOB_TYPE_NAME : JOB_TYPE.pa_process,# @UndefinedVariable
-                          UUID : cls._pa_process_ac_uuid,
-                          ARCHIVE : "2014-12-07beta9/20141207ablcontlowT61_2_25",
-                          DEVICE : "beta7",
-                          DYES : [ 
-                                  "594", 
-                                  "633", 
-                                  "cy5.5", 
-                                  "fam", 
-                                  "joe", 
-                                  "pe-cy7"
-                                 ],
-                          STATUS : "succeeded",
-                          SUBMIT_DATESTAMP : datetime.today(),
-                          START_DATESTAMP : datetime.today(),
-                          FINISH_DATESTAMP : datetime.today(),
-                          URL : "http://bioweb/results/8020/" + cls._pa_process_ac_uuid,
-                          CONFIG_URL : "http://bioweb/results/8020/" + cls._pa_process_ac_uuid + ".cfg",
-                          RESULT : os.path.join(_TEST_DIR, cls._pa_process_ac_uuid),
-                          CONFIG : os.path.join(_TEST_DIR, cls._pa_process_ac_uuid + ".cfg"),
-                         }
         ########################################################################
         # Primary Analysis Records
         ########################################################################
@@ -169,16 +134,52 @@ class Test(unittest.TestCase):
                               RESULT : os.path.join(_ARCHIVE_DIR, cls._abl_pa_uuid),
                               CONFIG : os.path.join(_ARCHIVE_DIR, cls._abl_pa_uuid + ".cfg"),
                              }
-        _DB_CONNECTOR.insert(PA_PROCESS_COLLECTION, [cls._ac_record, 
-                                                     cls._pa_record,
+        _DB_CONNECTOR.insert(PA_PROCESS_COLLECTION, [cls._pa_record,
                                                      cls._abl_pa_record])
+        
+        ########################################################################
+        # Identity Records
+        ########################################################################
+        cls._identity_uuid = "fb549af2-d807-492c-8b73-4f8c41435917"
+        cls._id_record = {
+                          FIDUCIAL_DYE: "joe",
+                          ASSAY_DYE: "fam",
+                          NUM_PROBES: 8,
+                          TRAINING_FACTOR: 1000,
+                          DYE_LEVELS : [ 
+                                  ["594", 2],
+                                  ["633", 2], 
+                                  ["cy5.5", 2],
+                                 ],
+                          IGNORED_DYES: [],
+                          FILTERED_DYES: ['pe'],
+                          PF_TRAINING_FACTOR: 10,
+                          UI_THRESHOLD: 4000,
+                          UUID : cls._identity_uuid,
+                          PA_PROCESS_UUID: cls._abl_pa_uuid,
+                          STATUS : "succeeded",
+                          JOB_NAME : "2016-01-28_05.22pm_beta_15-ddicara",
+                          JOB_TYPE_NAME : JOB_TYPE.sa_identity,# @UndefinedVariable
+                          SUBMIT_DATESTAMP : datetime.today(),
+                          CALC_DROP_PROB: False,
+                          START_DATESTAMP : datetime.today(),
+                          FINISH_DATESTAMP : datetime.today(),
+                          URL : "http://bioweb/results/8020/" + cls._identity_uuid,
+                          RESULT : os.path.join(_ARCHIVE_DIR, cls._identity_uuid),
+                          PLOT: os.path.join(_ARCHIVE_DIR, cls._identity_uuid + ".png"),
+                          PLOT_URL: "http://bioweb/results/8010/" +  cls._identity_uuid + ".png",
+                          REPORT: os.path.join(_ARCHIVE_DIR, cls._identity_uuid + ".yaml"),
+                          REPORT_URL: "http://bioweb/results/8010/" + cls._identity_uuid + ".yaml",
+                         }
+        _DB_CONNECTOR.insert(SA_IDENTITY_COLLECTION, [cls._id_record])
         
     @classmethod
     def tearDownClass(cls):
         _DB_CONNECTOR.remove(PA_PROCESS_COLLECTION, 
-                             {UUID: {"$in": [cls._pa_process_ac_uuid, 
-                                             cls._pa_uuid,
+                             {UUID: {"$in": [cls._pa_uuid,
                                              cls._abl_pa_record]}})
+        _DB_CONNECTOR.remove(SA_IDENTITY_COLLECTION, 
+                             {UUID: {"$in": [cls._identity_uuid]}})
         
     def setUp(self):
         self._client = app.test_client(self)
@@ -194,23 +195,11 @@ class Test(unittest.TestCase):
                         "Expected ABL identity result file doesn't exist: %s" % \
                         self._exp_abl_id_report_path)
         
-    def test_pa_results_exist(self):
+    def test_results_exist(self):
         """
         This tests that setUp properly inserts the primary analysis result in
         the database and that the result files exist.
         """
-        response = _DB_CONNECTOR.find_one(PA_PROCESS_COLLECTION, UUID,
-                                          self._pa_process_ac_uuid)
-
-        msg = "No entries in the DB with UUID = %s" % self._pa_process_ac_uuid
-        self.assertTrue(response, msg)
-
-        msg = "Result file cannot be found: %s" % response[RESULT]
-        self.assertTrue(os.path.isfile(response[RESULT]), msg)
-
-        msg = "Config file cannot be found: %s" % response[CONFIG]
-        self.assertTrue(os.path.isfile(response[CONFIG]), msg)
-
         response = _DB_CONNECTOR.find_one(PA_PROCESS_COLLECTION, UUID,
                                           self._pa_uuid)
 
@@ -235,76 +224,15 @@ class Test(unittest.TestCase):
         msg = "Config file cannot be found: %s" % response[CONFIG]
         self.assertTrue(os.path.isfile(response[CONFIG]), msg)
 
-#     def test_assay_caller(self):
-#         """
-#         Test the POST, GET and DELETE assay caller APIs.
-#         """
-#         # Construct url
-#         url = _ASSAY_CALLER_URL
-#         url = add_url_argument(url, UUID, self._ac_record[UUID], True)
-#         url = add_url_argument(url, JOB_NAME, _ASSAY_CALLER_JOB_NAME)
-#         url = add_url_argument(url, FIDUCIAL_DYE, _FIDUCIAL_DYE)
-#         url = add_url_argument(url, ASSAY_DYE, _ASSAY_DYE)
-#         url = add_url_argument(url, NUM_PROBES, _AC_NUM_PROBES)
-#         url = add_url_argument(url, TRAINING_FACTOR, _AC_TRAINING_FACTOR)
-#         url = add_url_argument(url, THRESHOLD, _THRESHOLD)
-#         url = add_url_argument(url, OUTLIERS, _OUTLIERS)
-#         url = add_url_argument(url, COV_TYPE, _COV_TYPE)
-# 
-#         # Submit identity job
-#         response          = post_data(self, url, 200)
-#         assay_caller_uuid = response[ASSAY_CALLER][0][UUID]
-# 
-#         # Test that submitting two jobs with the same name fails and returns
-#         # the appropriate error code.
-#         post_data(self, url, 403)
-# 
-#         running = True
-#         while running:
-#             time.sleep(10)
-#             response = get_data(self, _ASSAY_CALLER_URL, 200)
-#             for job in response[ASSAY_CALLER]:
-#                 if assay_caller_uuid == job[UUID]:
-#                     job_details = job
-#                     running     = job_details[STATUS] == 'running'
-# 
-#         # Copy result files to cwd for bamboo to ingest as artifacts
-#         assay_caller_txt_path = None
-#         if RESULT in job_details:
-#             assay_caller_txt_path = job_details[RESULT]
-#             if os.path.isfile(assay_caller_txt_path):
-#                 shutil.copy(assay_caller_txt_path, "observed_assay_caller.txt")
-# 
-#         kde_plot_path = None
-#         if KDE_PLOT in job_details:
-#             kde_plot_path = job_details[KDE_PLOT]
-#             if os.path.isfile(kde_plot_path):
-#                 shutil.copy(kde_plot_path, "observed_kde_plot.png")
-# 
-#         scatter_plot_path = None
-#         if SCATTER_PLOT in job_details:
-#             scatter_plot_path = job_details[SCATTER_PLOT]
-#             if os.path.isfile(scatter_plot_path):
-#                 shutil.copy(scatter_plot_path, "observed_scatter_plot.png")
-# 
-#         error = ""
-#         if 'error' in job_details:
-#             error = job_details['error']
-#         msg = "Expected sa assay caller job status succeeded, but found %s. " \
-#               "Error: %s" % (job_details[STATUS], error)
-#         self.assertEquals(job_details[STATUS], "succeeded", msg)
-# 
-#         # Delete sa assay caller job
-#         delete_url = add_url_argument(_ASSAY_CALLER_URL, UUID,
-#                                       assay_caller_uuid, True)
-#         delete_data(self, delete_url, 200)
-# 
-#         # Ensure job no longer exists in the database
-#         response = get_data(self, _ASSAY_CALLER_URL, 200)
-#         for job in response[ASSAY_CALLER]:
-#             msg = "PA process job %s still exists in database." % assay_caller_uuid
-#             self.assertNotEqual(assay_caller_uuid, job[UUID], msg)
-            
+        response = _DB_CONNECTOR.find_one(SA_IDENTITY_COLLECTION, UUID,
+                                          self._identity_uuid)
+
+        msg = "No entries in the DB with UUID = %s" % self._identity_uuid
+        self.assertTrue(response, msg)
+
+        msg = "Result file cannot be found: %s" % response[RESULT]
+        self.assertTrue(os.path.isfile(response[RESULT]), msg)
+
     def test_identity_on_golden_temple(self):
         """
         Test the POST, GET and DELETE identity APIs.
@@ -460,7 +388,74 @@ class Test(unittest.TestCase):
         for job in response[IDENTITY]:
             msg = "PA process job %s still exists in database." % identity_uuid
             self.assertNotEqual(identity_uuid, job[UUID], msg)
-
+            
+    def test_assay_caller(self):
+        """
+        Test the POST, GET and DELETE assay caller APIs.
+        """
+        # Construct url
+        url = _ASSAY_CALLER_URL
+        url = add_url_argument(url, UUID, self._id_record[UUID], True)
+        url = add_url_argument(url, JOB_NAME, _ASSAY_CALLER_JOB_NAME)
+        url = add_url_argument(url, FIDUCIAL_DYE, _FIDUCIAL_DYE)
+        url = add_url_argument(url, ASSAY_DYE, _ASSAY_DYE)
+        url = add_url_argument(url, NUM_PROBES, _AC_NUM_PROBES)
+        url = add_url_argument(url, TRAINING_FACTOR, AC_TRAINING_FACTOR)
+ 
+        # Submit identity job
+        response          = post_data(self, url, 200)
+        assay_caller_uuid = response[ASSAY_CALLER][0][UUID]
+ 
+        # Test that submitting two jobs with the same name fails and returns
+        # the appropriate error code.
+        post_data(self, url, 403)
+ 
+        running = True
+        while running:
+            time.sleep(10)
+            response = get_data(self, _ASSAY_CALLER_URL, 200)
+            for job in response[ASSAY_CALLER]:
+                if assay_caller_uuid == job[UUID]:
+                    job_details = job
+                    running     = job_details[STATUS] == 'running'
+ 
+        # Copy result files to cwd for bamboo to ingest as artifacts
+        assay_caller_txt_path = None
+        if RESULT in job_details:
+            assay_caller_txt_path = job_details[RESULT]
+            if os.path.isfile(assay_caller_txt_path):
+                shutil.copy(assay_caller_txt_path, "observed_assay_caller.txt")
+ 
+        kde_plot_path = None
+        if KDE_PLOT in job_details:
+            kde_plot_path = job_details[KDE_PLOT]
+            if os.path.isfile(kde_plot_path):
+                shutil.copy(kde_plot_path, "observed_kde_plot.png")
+ 
+        scatter_plot_path = None
+        if SCATTER_PLOT in job_details:
+            scatter_plot_path = job_details[SCATTER_PLOT]
+            if os.path.isfile(scatter_plot_path):
+                shutil.copy(scatter_plot_path, "observed_scatter_plot.png")
+ 
+        error = ""
+        if 'error' in job_details:
+            error = job_details['error']
+        msg = "Expected sa assay caller job status succeeded, but found %s. " \
+              "Error: %s" % (job_details[STATUS], error)
+        self.assertEquals(job_details[STATUS], "succeeded", msg)
+ 
+        # Delete sa assay caller job
+        delete_url = add_url_argument(_ASSAY_CALLER_URL, UUID,
+                                      assay_caller_uuid, True)
+        delete_data(self, delete_url, 200)
+ 
+        # Ensure job no longer exists in the database
+        response = get_data(self, _ASSAY_CALLER_URL, 200)
+        for job in response[ASSAY_CALLER]:
+            msg = "PA process job %s still exists in database." % assay_caller_uuid
+            self.assertNotEqual(assay_caller_uuid, job[UUID], msg)
+            
 #=============================================================================
 # Main
 #=============================================================================

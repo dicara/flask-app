984188aed9c6bd9acf79a76e53ce630fd8aefd6e
ddicara-gb <ddicara@gnubio.com>
2016-02-20 14:47:52 -0500

    Update secondary analysis to v1.2.1 which contains some identity and assay caller plotting updates.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _LONG_DESCRIPTION = open(_README).read()
 #===============================================================================
 setup(
       name             = 'bioweb-api',
-      version          = '2.1',
+      version          = '2.2',
       author           = 'Dan DiCara',
       author_email     = 'ddicara@gnubio.com', 
       entry_points     = {'console_scripts': [
@@ -56,7 +56,7 @@ setup(
                           'ncbi-utilities>=0.2',
                           'probe-design>=0.3',
                           'primary-analysis>=2.11',
-                          'secondary-analysis>=1.1.1',
+                          'secondary-analysis>=1.2.1',
                          ],
       package_data     = {'': ['bioweb_api/templates/*'],},
       description      = _LONG_DESCRIPTION,

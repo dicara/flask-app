5ca81a36f644baf30c59ccaee17430283ccb3249
yshenggb <ysheng@gnubio.com>
2016-06-20 11:20:27 -0400

    update secondary-analysis version to 1.43

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _LONG_DESCRIPTION = open(_README).read()
 #===============================================================================
 setup(
       name             = 'bioweb-api',
-      version          = '3.9.1',
+      version          = '3.10',
       author           = 'Dan DiCara',
       author_email     = 'ddicara@gnubio.com',
       entry_points     = {'console_scripts': [
@@ -56,7 +56,7 @@ setup(
                           'ncbi-utilities>=0.2',
                           'probe-design>=0.3',
                           'primary-analysis>=2.13',
-                          'secondary-analysis>=1.37',
+                          'secondary-analysis>=1.43',
                           'experiment-database>=0.6.1',
                           'PyPDF2>=1.25.1',
                           'reportlab>=3.3.0',

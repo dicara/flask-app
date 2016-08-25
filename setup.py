03a641354c77c4dd4d2997b2aac8dc4c96179b75
yshenggb <ysheng@gnubio.com>
2016-08-25 14:26:45 -0400

    sa version bump

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _LONG_DESCRIPTION = open(_README).read()
 #===============================================================================
 setup(
       name             = 'bioweb-api',
-      version          = '3.26',
+      version          = '3.27',
       author           = 'Dan DiCara',
       author_email     = 'ddicara@gnubio.com',
       entry_points     = {'console_scripts': [
@@ -57,7 +57,7 @@ setup(
                           'probe-design>=0.3',
                           'primary-analysis>=2.13',
                           'predator==0.1',
-                          'secondary-analysis==1.67.1',
+                          'secondary-analysis==1.69.2',
                           'experiment-database==0.10.1',
                           'PyPDF2>=1.25.1',
                           'reportlab>=3.3.0',

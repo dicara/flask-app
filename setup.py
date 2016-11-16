0faebd7f52a1b496559f159284df52b8a5c90126
nbrown <nbrown@gnubio.com>
2016-11-16 16:30:19 -0500

    updated version to 3.45.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _LONG_DESCRIPTION = open(_README).read()
 #===============================================================================
 setup(
       name             = 'bioweb-api',
-      version          = '3.44.0',
+      version          = '3.45.0',
       author           = 'Dan DiCara',
       author_email     = 'ddicara@gnubio.com',
       entry_points     = {'console_scripts': [

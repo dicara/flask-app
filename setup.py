fe333ec880ae5bf3f9bc4643ecee112dd002f168
yshenggb <ysheng@gnubio.com>
2017-07-07 14:02:04 -0700

    bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ _LONG_DESCRIPTION = open(_README).read()
 #===============================================================================
 setup(
       name             = 'bioweb-api',
-      version          = '3.94.1',
+      version          = '3.94.2',
       author           = 'Dan DiCara',
       author_email     = 'ddicara@gnubio.com',
       entry_points     = {'console_scripts': [

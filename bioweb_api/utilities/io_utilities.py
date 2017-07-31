373ff34940d58f80023819e27fcfe8fbef8749b6
yshenggb <ysheng@gnubio.com>
2017-07-31 18:59:00 +0000

    bugfix in deleting TSVs

--- bioweb_api/utilities/io_utilities.py
+++ bioweb_api/utilities/io_utilities.py
@@ -334,7 +334,7 @@ def delete_tsv(collection):
                 doc = job[doc_name]
                 if doc.get(URL) is not None:
                     silently_remove_file(url_to_filepath(doc[URL]))
-                    doc[URL] = None
+                    job[doc_name][URL] = None
         _DB_CONNECTOR.save(collection, job)
 
 def url_to_filepath(url):

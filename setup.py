48bd448b7d9389b2e8c8818e7eaede599c7c0934
nbrown <nbrown@gnubio.com>
2017-08-15 14:40:00 -0400

    updated secondary-analysis to version 3.32.0

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
                           'redis>=2.10.5',
                           'reportlab>=3.4.0',
                           'scikit-learn==0.18.0',
-                          'secondary-analysis>=3.29.0',
+                          'secondary-analysis>=3.32.0',
                           'simplejson>=3.10.0',
                           'tornado>=4.5.1',
                          ],
